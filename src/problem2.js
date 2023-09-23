419f1acdff40314645552c113f133ae49ff49dfa
Kim Da Eun <midekuna@gmail.com>
2023-09-23 16:47:15 +0900

    feat: problem2 solve

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,16 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+    const stack = [];
+
+    for (const element of cryptogram) {
+        let length = stack.length;
+
+        if (stack[length - 1] === element) {
+            stack.pop();
+        } else {
+            stack.push(element);
+        }
+    }
+    return stack.join("");
 }
 
 module.exports = problem2;

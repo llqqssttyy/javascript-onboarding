7b902159f4a29b1c2cf0392db13d567ed4168efe
Kim Da Eun <midekuna@gmail.com>
2023-09-23 17:01:29 +0900

    feat: problem3 solve

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
 function problem3(number) {
-  var answer;
-  return answer;
+    let clapCnt = 0;
+
+    for (num = 1; num <= number; num++) {
+        clapCnt += numOfClap(num);
+    }
+    return clapCnt;
+}
+
+function numOfClap(num) {
+    const str = num.toString();
+    const cnt3 = str.match(/3/g)?.length ?? 0;
+    const cnt6 = str.match(/6/g)?.length ?? 0;
+    const cnt9 = str.match(/9/g)?.length ?? 0;
+
+    return cnt3 + cnt6 + cnt9;
 }
 
 module.exports = problem3;

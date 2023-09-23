a4e39a12c8e4bbf710ade4a7d70d805c19d26350
Kim Da Eun <midekuna@gmail.com>
2023-09-24 00:22:03 +0900

    feat: problem5 solve

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,8 +1,8 @@
 // const problem1 = require("../src/problem1");
 // const problem2 = require("../src/problem2");
 // const problem3 = require("../src/problem3");
-const problem4 = require("../src/problem4");
-// const problem5 = require("../src/problem5");
+// const problem4 = require("../src/problem4");
+const problem5 = require("../src/problem5");
 // const problem6 = require("../src/problem6");
 // const problem7 = require("../src/problem7");
 
@@ -38,16 +38,17 @@ const problem4 = require("../src/problem4");
 //     });
 // });
 
-describe("problem4", () => {
+// describe("problem4", () => {
+//     test("case1", () => {
+//         expect(problem4("I love you")).toEqual("R olev blf");
+//     });
+// });
+
+describe("problem5", () => {
     test("case1", () => {
-        expect(problem4("I love you")).toEqual("R olev blf");
+        expect(problem5(50237)).toEqual([1, 0, 0, 0, 0, 2, 0, 3, 7]);
     });
 });
-
-// describe("problem5", () => {
-//   test("case1", () => {
-//     expect(problem5(50237)).toEqual([1, 0, 0, 0, 0, 2, 0, 3, 7]);
-//   });
 
 //   test("case2", () => {
 //     expect(problem5(15000)).toEqual([0, 1, 1, 0, 0, 0, 0, 0, 0]);
--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,22 @@
 function problem5(money) {
-  var answer;
-  return answer;
+    const atm = new CurrencyExchangeMachine(money);
+    return atm.exchange();
+}
+
+class CurrencyExchangeMachine {
+    constructor(money) {
+        this.money = money;
+        this.banknotes = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+    }
+    exchange() {
+        const billCnts = this.banknotes.map((bill) => this.getNumOfBills(bill));
+        return billCnts;
+    }
+    getNumOfBills(bill) {
+        const billCnt = Math.floor(this.money / bill); // 지폐 수
+        this.money = this.money % bill; // 잔돈
+        return billCnt;
+    }
 }
 
 module.exports = problem5;
